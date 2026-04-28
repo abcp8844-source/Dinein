f2e1a1e5cf8d5fd497d8360af5d3e8cdbbe3d762
abcp8844-source <abcp8844@gmail.com>
2026-04-28 23:28:22 +0700

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
+
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Ensure smooth asset loading for Android Preview Build
+config.resolver.sourceExts.push('js', 'json', 'ts', 'tsx', 'jsx');
 
 module.exports = config;
