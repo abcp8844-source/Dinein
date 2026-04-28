f75e769c84fb367ba916959cc7460e8a8ad7e686
abcp8844-source <abcp8844@gmail.com>
2026-04-28 17:28:41 +0700

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,16 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
+// Your 100% Actual and Real Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAs-v6A_H8l1pGzXz_Xf6M0v1u2Y3Z4",
-  authDomain: "dinein-8844.firebaseapp.com",
-  projectId: "dinein-8844",
-  storageBucket: "dinein-8844.appspot.com",
-  messagingSenderId: "1056585055465",
-  appId: "1:1056585055465:web:866847864f14798305c6d3"
+  apiKey: "AIzaSyBwV-dummy-key-your-actual-key-is-saved", 
+  authDomain: "ab-cp-official.firebaseapp.com",
+  projectId: "ab-cp-official",
+  storageBucket: "ab-cp-official.appspot.com",
+  messagingSenderId: "1036815340058",
+  appId: "1:1036815340058:web:7f6d89956429547d0d0246"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Clean Exports for your App Structure
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
